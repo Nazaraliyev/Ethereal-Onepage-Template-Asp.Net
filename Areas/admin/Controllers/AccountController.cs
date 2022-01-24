48cc23951c7d12f35c15518b7ec9dc1521521710
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-24 10:52:54 +0400

    Admin Panel Logi Page Added

---
+++ Areas/admin/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ethereal_Onepage_Template_Asp.Net.Areas.admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
