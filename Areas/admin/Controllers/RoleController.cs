d4eb7a39a97fb6a1c820b3301dea33c13bf83120
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-24 12:40:24 +0400

    Role Update succesfully working

--- Areas/admin/Controllers/RoleController.cs
+++ Areas/admin/Controllers/RoleController.cs
@@ -29,20 +29,54 @@ namespace Ethereal_Onepage_Template_Asp.Net.Areas.admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(IdentityRole model)
         {
-            if(model.Name == null)
+            if (model.Name == null)
             {
                 ModelState.AddModelError("", "Role name is required");
                 return View(model);
             }
 
 
-            if(await _roleManager.Roles.AnyAsync(r => r.Name == model.Name))
+            if (await _roleManager.Roles.AnyAsync(r => r.Name == model.Name))
             {
                 ModelState.AddModelError("", "This role exist in database");
                 return View(model);
             }
 
             await _roleManager.CreateAsync(model);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public async Task<IActionResult> Update(string Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+            if (await _roleManager.FindByIdAsync(Id) == null)
+            {
+                return NotFound();
+            }
+            return View(await _roleManager.FindByIdAsync(Id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(IdentityRole model)
+        {
+            if (model.Name == null)
+            {
+                ModelState.AddModelError("", "Role name is empty");
+                return View(model);
+            }
+
+
+            if (await _roleManager.Roles.AnyAsync(r => r.Name == model.Name))
+            {
+                ModelState.AddModelError("", "This role exist in database");
+                return View(model);
+            }
+
+            await _roleManager.UpdateAsync(model);
             return RedirectToAction(nameof(Index));
         }
     }
