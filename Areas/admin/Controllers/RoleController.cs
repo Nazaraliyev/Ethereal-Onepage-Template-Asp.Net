84fb512aa49af82527121c0226b3343b943fa5a4
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-24 12:29:32 +0400

    Role Create succesfully working

--- Areas/admin/Controllers/RoleController.cs
+++ Areas/admin/Controllers/RoleController.cs
@@ -24,5 +24,26 @@ namespace Ethereal_Onepage_Template_Asp.Net.Areas.admin.Controllers
         {
             return View();
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole model)
+        {
+            if(model.Name == null)
+            {
+                ModelState.AddModelError("", "Role name is required");
+                return View(model);
+            }
+
+
+            if(await _roleManager.Roles.AnyAsync(r => r.Name == model.Name))
+            {
+                ModelState.AddModelError("", "This role exist in database");
+                return View(model);
+            }
+
+            await _roleManager.CreateAsync(model);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
