79e2eedf89f64fa392b4f1bea3d5ae76f35dc704
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-24 12:18:36 +0400

    Role controller created adn roles index page is ready

---
+++ Areas/admin/Controllers/RoleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Ethereal_Onepage_Template_Asp.Net.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _roleManager.Roles.ToListAsync());
+        }
+    }
+}
